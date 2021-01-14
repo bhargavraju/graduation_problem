d203fc705031c272dfba6b9cb622de51c331a37f
Bhargav Raju Jampani <jampanibhargavraju@gmail.com>
2021-01-14 17:46:36 +0530

    Code to generate all valid sequences

---
+++ generate_valid_sequences.py
@@ -0,0 +1,18 @@
+def generate_valid_sequences(n):
+    ans = []
+
+    def rec_helper(curr, res):
+        if len(curr) == n:
+            res.append(curr)
+            return
+
+        if not (len(curr) >= 2 and curr[-2:] == [0, 0]):
+            rec_helper(curr + [0], res)
+        rec_helper(curr + [1], res)
+
+    rec_helper([], ans)
+    print(ans)
+    print(len(ans))
+
+
+generate_valid_sequences(7)
